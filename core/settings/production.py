37657a09da2791bc8e720f76b6c152b222fcc2f0
André Izarra <programador385@gmail.com>
2021-09-21 11:51:39 -0400

    heroku settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,4 +1,6 @@
 from .base import *
+import os
+import django_heroku
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -11,12 +13,13 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER': 'user1',
-        'PASSWORD': 'j2osqcdm',
-        'HOST': 'localhost',
+        'NAME': 'dec61ctb4p23n4',
+        'USER': 'khbeyqjumrddsd',
+        'PASSWORD': '712a305abf733eb857ce2f3b9415e256fa129e5b2ac2a33871cbbc01f6095617',
+        'HOST': 'ec2-44-193-150-214.compute-1.amazonaws.com',
         'PORT': 5432,
     }
 }
 
 STATIC_URL = '/static/'
+django_heroku.setttings(locals())
